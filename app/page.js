7b27a20858fd07d5ef8cf463bca2d2531b1a7a62
renzorainier <114910204+renzorainier@users.noreply.github.com>
2025-10-27 11:52:06 +0800

    installable version

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import LandingPage from "./landing/page";
+import Head from "next/head.js";
 
 // import PoseEstimator from './PoseEstimator';
 // import DepthEstimator from './DepthEstimator';
@@ -13,11 +14,20 @@ import LandingPage from "./landing/page";
 function App() {
   return (
     <div>
+      <Head>
+
+       <link rel="manifest" href="/manifest.json" />
+           <title>Tindig</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Tindig" />
+        {/* <meta property="og:image" content="https://dvbs.vercel.app/target.png" /> */}
+        {/* Add more Open Graph meta tags as needed */}
+      </Head>
       <header>
-        {/* <PoseEstimator /> */}
-        {/* <DepthEstimator /> */}
         <LandingPage />
-        {/* <WebcamTest /> */}
+
       </header>
     </div>
   );
